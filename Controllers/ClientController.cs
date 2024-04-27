eee4bbb6ba64ffa43528cffe2847d71cca29ab98
wael <wael.abdallah@etudiant-enit.utm.tn>
2024-04-27 13:31:49 +0100

    fixing create client issue

--- Controllers/ClientController.cs
+++ Controllers/ClientController.cs
@@ -41,6 +41,14 @@ namespace hotelManagementSystem20.Controllers
         {
             if (ModelState.IsValid)
             {
+                
+                var nextValQuery = "SELECT NEXT VALUE FOR client_seq;";
+                var nextVal = db.Database.SqlQuery<long>(nextValQuery).FirstOrDefault();
+
+               
+                client.client_id = (int)nextVal;
+
+               
                 db.client.Add(client);
                 db.SaveChanges();
                 return RedirectToAction("Index");
