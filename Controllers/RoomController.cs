f2a31ec82ee7d9804891e36ee793c4fd3670030a
wael <wael.abdallah@etudiant-enit.utm.tn>
2024-04-30 15:27:45 +0100

    mm

--- Controllers/RoomController.cs
+++ Controllers/RoomController.cs
@@ -1,120 +1,120 @@
-﻿using System.Data.Entity;
-using System.Linq;
-using System.Net;
-using System.Web.Mvc;
-using hotelManagementSystem20.models;
+﻿    using System.Data.Entity;
+    using System.Linq;
+    using System.Net;
+    using System.Web.Mvc;
+    using hotelManagementSystem20.models;
 
-namespace hotelManagementSystem20.Controllers
-{
-    public class RoomController : Controller
+    namespace hotelManagementSystem20.Controllers
     {
-        private HotelManagementSystemEntities4 db = new HotelManagementSystemEntities4();
-
-        // GET: Room
-        public ActionResult Index()
+        public class RoomController : Controller
         {
-            var rooms = db.room.ToList();
-            return View(rooms);
-        }
+            private HotelManagementSystemEntities4 db = new HotelManagementSystemEntities4();
 
-        // GET: Room/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
+            // GET: Room
+            public ActionResult Index()
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                var rooms = db.room.ToList();
+                return View(rooms);
             }
-            room room = db.room.Find(id);
-            if (room == null)
+
+            // GET: Room/Details/5
+            public ActionResult Details(int? id)
             {
-                return HttpNotFound();
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                room room = db.room.Find(id);
+                if (room == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(room);
             }
-            return View(room);
-        }
 
-        // GET: Room/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Room/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(room room)
-        {
-            if (ModelState.IsValid)
+            // GET: Room/Create
+            public ActionResult Create()
             {
-                
-                var nextValQuery = "SELECT NEXT VALUE FOR room_seq;";
-                var nextVal = db.Database.SqlQuery<long>(nextValQuery).FirstOrDefault();
+                return View();
+            }
 
-                
-                room.room_id = (int)nextVal;
+            // POST: Room/Create
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public ActionResult Create(room room)
+            {
+                if (ModelState.IsValid)
+                {
+                    
+                    var nextValQuery = "SELECT NEXT VALUE FOR room_seq;";
+                    var nextVal = db.Database.SqlQuery<long>(nextValQuery).FirstOrDefault();
 
-                
-                db.room.Add(room);
+                    
+                    room.room_id = (int)nextVal;
+
+                    
+                    db.room.Add(room);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+                return View(room);
+            }
+        
+
+        // GET: Room/Edit/5
+        public ActionResult Edit(int? id)
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                room room = db.room.Find(id);
+                if (room == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(room);
+            }
+
+            // POST: Room/Edit/5
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public ActionResult Edit(room room)
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Entry(room).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(room);
+            }
+
+            // GET: Room/Delete/5
+            public ActionResult Delete(int? id)
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                room room = db.room.Find(id);
+                if (room == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(room);
+            }
+
+            // POST: Room/Delete/5
+            [HttpPost, ActionName("Delete")]
+            [ValidateAntiForgeryToken]
+            public ActionResult DeleteConfirmed(int id)
+            {
+                room room = db.room.Find(id);
+                db.room.Remove(room);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-
-            return View(room);
-        }
-    
-
-    // GET: Room/Edit/5
-    public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            room room = db.room.Find(id);
-            if (room == null)
-            {
-                return HttpNotFound();
-            }
-            return View(room);
-        }
-
-        // POST: Room/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(room room)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(room).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(room);
-        }
-
-        // GET: Room/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            room room = db.room.Find(id);
-            if (room == null)
-            {
-                return HttpNotFound();
-            }
-            return View(room);
-        }
-
-        // POST: Room/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            room room = db.room.Find(id);
-            db.room.Remove(room);
-            db.SaveChanges();
-            return RedirectToAction("Index");
         }
     }
-}
