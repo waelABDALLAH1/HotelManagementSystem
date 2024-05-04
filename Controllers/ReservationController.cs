c596ff19c9400051e51f6d965e04befd62e64fd4
rania1231 <raniahamdi3112@gmail.com>
2024-05-04 10:27:07 +0100

    reservation version 1

--- Controllers/ReservationController.cs
+++ Controllers/ReservationController.cs
@@ -1,7 +1,9 @@
 ﻿using hotelManagementSystem20.models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -26,6 +28,93 @@ namespace hotelManagementSystem20.Controllers
             }
             return View(reservation);
         }
+
+        // GET: Reservation/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Reservation/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Reservation reservation)
+        {
+            if (ModelState.IsValid)
+            {
+
+                var nextValQuery = "SELECT NEXT VALUE FOR Reservation_seq;";
+                var nextVal = db.Database.SqlQuery<long>(nextValQuery).FirstOrDefault();
+
+
+                reservation.Reservation_Id = (int)nextVal;
+
+
+                db.Reservation.Add(reservation);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(reservation);
+        }
+
+
+        // GET: Reservation/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Reservation reservation = db.Reservation.Find(id);
+            if (reservation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(reservation);
+        }
+
+        // POST: Reservation/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Reservation reservation)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(reservation).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(reservation);
+        }
+
+        // GET: Reservation/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Reservation reservation = db.Reservation.Find(id);
+            if (reservation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(reservation);
+        }
+
+        // POST: Reservation/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Reservation reservation = db.Reservation.Find(id);
+            db.Reservation.Remove(reservation);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
 
 
 
